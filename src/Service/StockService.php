19a5b29d5b82f2d6a3f5837a2ad9fc7d3aa2befb
Matt <61901583+zewas1@users.noreply.github.com>
2022-01-02 21:07:15 +0200

    refactoring

--- src/Service/StockService.php
+++ src/Service/StockService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Builder\StockBuilder;
 use App\Entity\StockInformation as Stock;
 use GuzzleHttp\Exception\GuzzleException;
 
@@ -15,10 +16,18 @@ class StockService
     private ApiCommunicationService $api;
 
     /**
-     * @param ApiCommunicationService $api
+     * @var StockBuilder
      */
-    public function __construct(ApiCommunicationService $api){
-        $this->api=$api;
+    private StockBuilder $builder;
+
+    /**
+     * @param ApiCommunicationService $api
+     * @param StockBuilder $builder
+     */
+    public function __construct(ApiCommunicationService $api, StockBuilder $builder)
+    {
+        $this->api = $api;
+        $this->builder = $builder;
     }
 
     /**
@@ -33,29 +42,6 @@ class StockService
         $response = $this->api->makeCall($stock);
         $array = json_decode($response, true);
 
-        return $this->arrayToEntity($array);
-    }
-
-    /**
-     * @param $response
-     *
-     * @return Stock
-     */
-    public function arrayToEntity($response): Stock
-    {
-        $stock = new Stock();
-
-        $stock->setChange($response['change']);
-        $stock->setChangePercent($response['changePercent']);
-        $stock->setCompanyName($response['companyName']);
-        $stock->setCurrency($response['currency']);
-        $stock->setLatestPrice($response['latestPrice']);
-        $stock->setSymbol($response['symbol']);
-        $stock->setYearHigh($response['week52High']);
-        $stock->setYearLow($response['week52Low']);
-        $stock->setYtdChange($response['ytdChange']);
-        $stock->setIsUsMarketOpen($response['isUSMarketOpen']);
-
-        return $stock;
+        return $this->builder->buildStock($array);
     }
 }
